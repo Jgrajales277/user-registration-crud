262ff82af4c7f0d094c56869ecb85200cd6de608
“Juliana <jgrajales277@gmail.com>
2023-02-19 20:23:04 -0500

    Creat rutas de  home y producto, agregar foter y header con dropdown menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 import { IngresoComponent } from './security/ingreso/ingreso.component';
 import { RegistroComponent } from './security/registro/registro.component';
+import { ProductsComponent } from './pages/products/products.component';
 
 const app_routes: Routes = [
+  { path: 'home', component:HomeComponent },
   { path: 'ingreso', component: IngresoComponent },
   { path: 'registro', component: RegistroComponent },
-  { path: '**', redirectTo: 'ingreso', pathMatch: 'full' },
+  { path: 'productos', component: ProductsComponent},
+  { path: '**', redirectTo: 'home', pathMatch: 'full' },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http'
 
 //rutas
 
@@ -8,18 +9,27 @@ import { AppComponent } from './app.component';
 import { IngresoComponent } from './security/ingreso/ingreso.component';
 import { RegistroComponent } from './security/registro/registro.component';
 import { ApproutingModule } from './app-routing.module';
-
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { ProductsComponent } from './pages/products/products.component';
 
   
 @NgModule({
   declarations: [
     AppComponent,
     IngresoComponent,
-    RegistroComponent
+    RegistroComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProductsComponent,
   ],
   imports: [
     BrowserModule,
     ApproutingModule,
+    HttpClientModule,
+    
     
 
   ],
@@ -27,3 +37,5 @@ import { ApproutingModule } from './app-routing.module';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
