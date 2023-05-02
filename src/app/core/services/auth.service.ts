fff0f20734139810b2d16ac2eb5d047d49b98fa3
“Juliana <jgrajales277@gmail.com>
2023-05-02 13:09:44 -0500

    SignIn funcional

---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Router } from '@angular/router';
+import { User } from '../interfaces/user';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(
+    public afStore: AngularFirestore,
+    public afAuth: AngularFireAuth,
+    private router: Router
+  ) {}
+
+  signIn(user: User) {
+    const { email, password } = user;
+    return this.afAuth
+      .signInWithEmailAndPassword(email, password)
+      .then((resp) => {
+        console.log(resp);
+      })
+      .catch((error) => {
+        console.error(error);
+        // this.router.navigateByUrl('/members');
+        this.router.navigate(['/Members']);
+
+      });
+  }
+
+  signUp(user: User) {
+    const { email, password } = user;
+    return this.afAuth
+      .createUserWithEmailAndPassword(email, password)
+      .then((resp) => {
+        console.log(resp);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+}
--- src/app/security/signin/signin.component.ts
+++ src/app/security/signin/signin.component.ts
@@ -1,11 +1,25 @@
-
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { User } from '../../core/interfaces/user';
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
-  styleUrls: ['./signin.component.css']
+  styleUrls: ['./signin.component.css'],
 })
 export class SigninComponent {
+  user: User = {email:"", password:""};
+  constructor(private auth: AuthService, private router: Router) {}
 
+  signIn(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+
+    this.auth.signIn(this.user);
+    
+  }
 }
